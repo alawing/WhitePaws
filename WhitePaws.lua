a954ff67f20e631040dcb2d1d8d4ddbf13f7a2ff
Disney <feibilanceon@gmail.com>
2022-11-14 01:18:37 +0800

    Update WhitePaws.lua

--- WhitePaws.lua
+++ WhitePaws.lua
@@ -57,7 +57,7 @@ end
 
 --变形金刚-变猫
 function bxjgm(e)
-	if not wc.strongControl and wc.enoughMana() and (wc.needUnroot() or (wc.enoughEnergy(e) and wc.getRage() < 25)) then
+	if not wc.strongControl and wc.enoughMana() and (wc.needUnroot() or wc.enoughEnergy(e)) then
 		if not wc.getShiftGCD() then
 			SetCVar('autoUnshift', 1)
 		else
